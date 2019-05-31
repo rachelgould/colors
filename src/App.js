1a42babdda62a262331296fec907bfcee50a1827
Rachel Gould <rachel@rachelgould.dev>
2019-05-31 15:26:29 -0700

    grid of random colors appears on load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const makeRandomColor = () => {
+    let allowable = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let hexArr = ['#'];
+    for (let i = 0; i < 6; i++) {
+      hexArr.push(allowable[Math.floor(Math.random() * 16) + 0]);
+    }
+    return hexArr.join("");  
+  }
+
+  const makeBoxes = () => {
+    let boxes = [];
+    for (let i = 0; i < 200; i++) {
+      let newColor = makeRandomColor();
+      let style = { backgroundColor: newColor }
+      boxes.push(
+        <div className="colored-box" style={style}>{newColor}</div>
+      )
+    }
+    return boxes
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="boxes">
+          {makeBoxes()}
+        </div>
       </header>
     </div>
   );
